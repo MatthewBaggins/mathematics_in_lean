9fc83ab7f9e56d9e86cb537f752eee40141fc79a
Mateusz Bagiński <bagginsmatthew@gmail.com>
2025-02-18 21:50:16 +0200

    3.5 done

--- MIL/C03_Logic/S05_Disjunction.lean
+++ MIL/C03_Logic/S05_Disjunction.lean
@@ -24,7 +24,9 @@ example (h : y < -1) : y > 0 ∨ y < -1 :=
 example : x < |y| → x < y ∨ x < -y := by
   rcases le_or_gt 0 y with h | h
   · rw [abs_of_nonneg h]
-    intro h; left; exact h
+    intro h
+    left
+    exact h
   · rw [abs_of_neg h]
     intro h; right; exact h
 
@@ -58,19 +60,64 @@ example : x < |y| → x < y ∨ x < -y := by
 namespace MyAbs
 
 theorem le_abs_self (x : ℝ) : x ≤ |x| := by
-  sorry
+  match le_or_gt 0 x with
+    | Or.inl h =>
+      rw [abs_of_nonneg h]
+    | Or.inr h =>
+      rw [abs_of_neg h]
+      linarith
+
 
 theorem neg_le_abs_self (x : ℝ) : -x ≤ |x| := by
-  sorry
+  match le_or_gt 0 x with
+    | Or.inl h =>
+      rw [abs_of_nonneg h]
+      linarith
+    | Or.inr h =>
+      rw [abs_of_neg h]
+
 
 theorem abs_add (x y : ℝ) : |x + y| ≤ |x| + |y| := by
-  sorry
+  rcases le_or_gt 0 (x+y) with h | h
+  . rw [abs_of_nonneg h]
+    linarith [le_abs_self x, le_abs_self y]
+  . rw [abs_of_neg h]
+    linarith [neg_le_abs_self x, neg_le_abs_self y]
 
 theorem lt_abs : x < |y| ↔ x < y ∨ x < -y := by
-  sorry
+  rcases le_or_gt 0 y with h | h
+  . rw [abs_of_nonneg h]
+    constructor
+    . intro xlty; left; exact xlty
+    . intro h'
+      rcases h' with xlty | xltny
+      . exact xlty
+      . linarith
+  . rw [abs_of_neg h]
+    constructor
+    . intro xltny; right; exact xltny
+    . intro h'
+      rcases h' with xlty | xltny
+      linarith
+      linarith
+
 
 theorem abs_lt : |x| < y ↔ -y < x ∧ x < y := by
-  sorry
+  rcases le_or_gt 0 x with h | h
+  . rw [abs_of_nonneg h]
+    constructor
+    . intro xlty
+      constructor
+      . linarith
+      . assumption
+    . rintro ⟨h', h''⟩; exact h''
+  . rw [abs_of_neg h]
+    constructor
+    . intro h
+      constructor
+      linarith
+      linarith
+    . intro ⟨h', h''⟩; linarith
 
 end MyAbs
 
@@ -80,7 +127,7 @@ example {x : ℝ} (h : x ≠ 0) : x < 0 ∨ x > 0 := by
   rcases lt_trichotomy x 0 with xlt | xeq | xgt
   · left
     exact xlt
-  · contradiction
+  · exfalso; apply h xeq -- contradiction
   · right; exact xgt
 
 example {m n k : ℕ} (h : m ∣ n ∨ m ∣ k) : m ∣ n * k := by
@@ -90,24 +137,49 @@ example {m n k : ℕ} (h : m ∣ n ∨ m ∣ k) : m ∣ n * k := by
   · rw [mul_comm, mul_assoc]
     apply dvd_mul_right
 
+-- example {m n k : ℕ} (h : m ∣ n ∨ m ∣ k) : m ∣ n * k := by
+--   rcases h with ⟨a, ha⟩ | ⟨a', ha'⟩
+--   . rw [mul_assoc]
+
+
 example {z : ℝ} (h : ∃ x y, z = x ^ 2 + y ^ 2 ∨ z = x ^ 2 + y ^ 2 + 1) : z ≥ 0 := by
-  sorry
+  rcases h with ⟨x, y, rfl | rfl⟩
+  repeat
+  linarith [sq_nonneg x, sq_nonneg y]
+
 
 example {x : ℝ} (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  have h' : x^2 - 1 = 0 := by linarith
+  have h'' : (x + 1) * (x - 1) = 0 := by rw [← h']; ring
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h'' with h_ | h_
+  . right; linarith
+  . left; linarith
+
 
 example {x y : ℝ} (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  have h' : x^2 - y^2 = 0 := by linarith
+  have h'' : (x + y) * (x - y) = 0 := by linarith
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero h'' with h_ | h_
+  . right; linarith
+  . left; linarith
 
 section
 variable {R : Type*} [CommRing R] [IsDomain R]
 variable (x y : R)
 
 example (h : x ^ 2 = 1) : x = 1 ∨ x = -1 := by
-  sorry
+  have : x^2 - 1 = 0 := by rw [← h, sub_self]
+  have : (x + 1) * (x - 1) = 0 := by rw [← this]; ring
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero this with h' | h'
+  . right; apply eq_neg_iff_add_eq_zero.mpr h'
+  . left; apply eq_of_sub_eq_zero h'
 
 example (h : x ^ 2 = y ^ 2) : x = y ∨ x = -y := by
-  sorry
+  have : x^2 - y^2 = 0 := by rw [← h, sub_self]
+  have : (x + y) * (x - y) = 0 := by rw [← this]; ring
+  rcases eq_zero_or_eq_zero_of_mul_eq_zero this with h | h
+  . right; exact eq_neg_iff_add_eq_zero.mpr h
+  . left; exact eq_of_sub_eq_zero h
 
 end
 
@@ -124,5 +196,13 @@ example (P : Prop) : ¬¬P → P := by
   contradiction
 
 example (P Q : Prop) : P → Q ↔ ¬P ∨ Q := by
-  sorry
-
+  constructor
+  . intro hpq
+    by_cases hp : P
+    . right; exact hpq hp
+    . left; exact hp
+  . intro h
+    intro hp
+    rcases h with hnp | hq
+    . exfalso; exact hnp hp
+    . exact hq
